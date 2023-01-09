14d67bbc5e51317ed733f5bdeb531099a1caf61c
Sergey Tishchenko <seti.volch@gmail.com>
2023-01-09 19:51:05 +0700

    upgrade Squirrel

--- src/Squirrel.java
+++ src/Squirrel.java
@@ -1,9 +1,8 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 public class Squirrel {
-    int[] nuts;
+    private final int[] nuts;
+    private int maxNutsInt = 0;
     Squirrel(int[] nuts) {
         this.nuts = nuts;
     }
@@ -13,33 +12,34 @@ public class Squirrel {
     }
 
     public int maxNuts() {
-        int maxNutsInt = 0;
+        maxNutsInt = 0;
         for (int i = 0; i < nuts.length; i++) {
             if ((i + 1) < nuts.length) {
                 if ((i + 2) < nuts.length && nuts[i] < 0 && nuts[i + 1] < 0 && nuts[i + 2] < 0) {
                     if ((nuts[i] > nuts[i + 1] || (nuts[i] >= 0)) && (nuts[i] + nuts[i + 2]) > nuts[i + 1]) {
-                        maxNutsInt += nuts[i];
-                        maxNutsInt = Math.max(maxNutsInt, 0);
+                        jumpToIndex(i);
                     } else {
-                        maxNutsInt += nuts[i + 1];
+                        jumpToIndex(i + 1);
                         i++;
-                        maxNutsInt = Math.max(maxNutsInt, 0);
                     }
                 } else if (nuts[i] > nuts[i + 1] || (nuts[i] >= 0)) {
-                    maxNutsInt += nuts[i];
-                    maxNutsInt = Math.max(maxNutsInt, 0);
+                    jumpToIndex(i);
                 } else {
-                    maxNutsInt += nuts[i + 1];
+                    jumpToIndex(i + 1);
                     i++;
-                    maxNutsInt = Math.max(maxNutsInt, 0);
                 }
             } else if (nuts[i] >= 0) {
-                maxNutsInt += nuts[i];
-                maxNutsInt = Math.max(maxNutsInt, 0);
+                jumpToIndex(i);
             }
         }
             return maxNutsInt;
     }
+
+    private void jumpToIndex(int index) {
+        maxNutsInt += nuts[index];
+        maxNutsInt = Math.max(maxNutsInt, 0);
+    }
+
     @Test
     public static void main(String[] args) {
         inputAndPrintSquirrelMaxNuts(3, new int[]{1, -1, 2, -2, -7, -7, 2, 1});
@@ -52,7 +52,7 @@ public class Squirrel {
         inputAndPrintSquirrelMaxNuts(11, new int[]{1, -2, 4, 6, 3, -2, -2, -3, -1});
         inputAndPrintSquirrelMaxNuts(5, new int[]{1, 1, 1, 1, 1});
         inputAndPrintSquirrelMaxNuts(4, new int[]{1, 1, 1, 1, -1});
-        System.out.println("");
+        System.out.println(" ");
         inputAndPrintSquirrelMaxNuts(6, new int[]{2, 5, -3, -1});
         inputAndPrintSquirrelMaxNuts(9, new int[]{1, -2, 3, 1, -1, -4, 2, 3});
         inputAndPrintSquirrelMaxNuts(23, new int[]{10, -2, 3, 1, -1, -4, 2, 3, 2, 2, -2, -3, -100, -10, -1, 2, 10, -2, 3, -1, -2});
